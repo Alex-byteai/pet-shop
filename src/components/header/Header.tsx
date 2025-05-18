7e543fcb6d5f5f2fe5c2fe50357a02d7587aad3d
Alex-byteai <20223759@aloe.ulima.edu.pe>
2025-05-18 16:58:01 -0600

    Alumno 1: limpie el codigo no utlizado y actualize el elemento de busqueda

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,44 +1,5 @@
-// import { Link } from 'react-router-dom';
-
-// import './Header.css'; 
-
-// const Header = () => {
-//   return (
-//     <header className="header">
-//       <div className="header-container">
-        
-//         <div className="logo-container">
-//           <Link to="/" className="logo">
-//             PetShop
-//           </Link>
-//         </div>
-        
-//         <div className="search-container">
-//           <input 
-//             type="text" 
-//             placeholder="Buscar productos, marcas y más..."
-//             className="search-input"
-//           />
-//           <button className="search-button">
-//             Buscar
-//           </button>
-//         </div>
-
-//         <nav className="nav-menu">
-//           <ul className='main-menu'>
-//             <Link to="/cart" className="nav-link">Carrito</Link>
-//             <Link to="/login" className="nav-link">Mi cuenta</Link>
-//           </ul>
-//         </nav>
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default Header;
-
 import { Link } from 'react-router-dom';
-import './Header.css'; // Asegúrate de importar los estilos
+import './Header.css'; 
 
 const Header = () => {
   return (
@@ -49,12 +10,18 @@ const Header = () => {
         </div>
 
         <div className="search-container">
+          <svg className="icon" 
+            aria-hidden="true" 
+            viewBox="0 0 24 24"><g>
+            <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
+            </path></g>
+          </svg>
           <input 
-            type="text" 
+            type="search" 
             placeholder="Buscar productos, marcas y más..."
             className="search-input"
           />
-          <button className="search-button">Buscar</button>
+          {/* <button className="search-button">Buscar</button> */}
         </div>
 
         <nav>
