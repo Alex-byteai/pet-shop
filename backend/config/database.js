10e9a725b36589b4b8805ff3886a4b13d1dd6bff
Yotzuga <anthony0456789@gmail.com>
2025-07-03 14:30:34 -0500

    s

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('demobd', 'postgres', 'fenix0456', {
+export const sequelize = new Sequelize('demobd', 'postgres', '123456', {
     host: 'localhost',
     dialect: 'postgres'
 });
